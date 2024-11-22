bea71ecaf7e11d536ff0e878acaad80797e8c03d
Nikita Podvirnyi <krypt0nn@vk.com>
2024-11-22 23:48:41 +0200

    feat(tasks): removed async handler functions

--- src/filesystem/tasks/handler.rs
+++ src/filesystem/tasks/handler.rs
@@ -25,24 +25,9 @@ impl FilesystemTasksHandler {
         Ok(())
     }
 
-    /// Send filesystem task to the scheduler with specified priority.
-    pub async fn send_async(&self, task: FilesystemTask, priority: FilesystemTaskPriority) -> anyhow::Result<()> {
-        self.sender.send_async(FilesystemSchedulerTask::PushTask {
-            task,
-            priority
-        }).await?;
-
-        Ok(())
-    }
-
     /// Send filesystem task to the scheduler with highest priority.
     pub fn send_high(&self, task: FilesystemTask) -> anyhow::Result<()> {
         self.send(task, FilesystemTaskPriority::High)
-    }
-
-    /// Send filesystem task to the scheduler with highest priority.
-    pub async fn send_high_async(&self, task: FilesystemTask) -> anyhow::Result<()> {
-        self.send_async(task, FilesystemTaskPriority::High).await
     }
 
     /// Send filesystem task to the scheduler with normal priority.
@@ -50,19 +35,9 @@ impl FilesystemTasksHandler {
         self.send(task, FilesystemTaskPriority::Normal)
     }
 
-    /// Send filesystem task to the scheduler with normal priority.
-    pub async fn send_normal_async(&self, task: FilesystemTask) -> anyhow::Result<()> {
-        self.send_async(task, FilesystemTaskPriority::Normal).await
-    }
-
     /// Send filesystem task to the scheduler with lowest priority.
     pub fn send_low(&self, task: FilesystemTask) -> anyhow::Result<()> {
         self.send(task, FilesystemTaskPriority::Low)
-    }
-
-    /// Send filesystem task to the scheduler with lowest priority.
-    pub async fn send_low_async(&self, task: FilesystemTask) -> anyhow::Result<()> {
-        self.send_async(task, FilesystemTaskPriority::Low).await
     }
 
     /// Poll filesystem task from the scheduler.
@@ -72,14 +47,5 @@ impl FilesystemTasksHandler {
         self.sender.send(FilesystemSchedulerTask::PollTask(send))?;
 
         Ok(recv.recv()?)
-    }
-
-    /// Poll filesystem task from the scheduler.
-    pub async fn poll_async(&self) -> anyhow::Result<FilesystemTask> {
-        let (send, recv) = flume::bounded(1);
-
-        self.sender.send_async(FilesystemSchedulerTask::PollTask(send)).await?;
-
-        Ok(recv.recv_async().await?)
     }
 }
